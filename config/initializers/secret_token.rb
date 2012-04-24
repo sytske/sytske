7274665920a97cb884f09d142fd6c70928aecf82
S. Haagsma <info@sytsmedia.nl>
2012-04-24 12:06:56 +0200

    testje

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sytske::Application.config.secret_token = '2f8b070f29ba355399bb06d4de6018cad1e2b37b91ecba8193f14276c0709371004115e985fdb6a18d6210f1396a40d8de6ae67a8e5377215a77b4782cf29d57'
